cbe6831065c748011b4f47f12e72522e3e4df791
Thomas Darimont <thomas.darimont@gmail.com>
2015-08-10 22:41:46 +0200

    Step 6 - Add custom base abstractions.
    Provided base abstraction for read-only Repositories via the ReadOnlyRepository interface.
    It also fixes the the type of the ID property to Long. Added Product domain class, sample data as well as an extension of ReadOnlyRepository todeclare a query method to find Product instances containing a given description.

--- spring-boot-data-repositories/src/main/resources/data.sql
+++ spring-boot-data-repositories/src/main/resources/data.sql
@@ -4,3 +4,7 @@ insert into Customer (id, email, firstname, lastname) values (3, 'ralf@example.o
 
 insert into Address (id, street, city, country, customer_id) values (1, 'Musterweg 10', 'Kaiserslautern', 'Germany', 1);
 insert into Address (id, street, city, country, customer_id) values (2, 'Am Eck 12', 'Kaiserslautern', 'Germany', 2);
+
+insert into Product (id, name, description, price) values (1, 'iPad', 'Apple tablet device', 499.0);
+insert into Product (id, name, description, price) values (2, 'MacBook Pro', 'Apple notebook', 1299.0);
+insert into Product (id, name, description, price) values (3, 'Dock', 'Dock for iPhone/iPad', 49.0);
---
+++ spring-boot-data-repositories/src/test/java/demo/ProductRepositoryIntegrationTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Integration tests for {@link ProductRepository}.
+ * 
+ * @author Oliver Gierke
+ * @author Thomas Darimont
+ * @since Step 6
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@SpringApplicationConfiguration(classes = App.class)
+public class ProductRepositoryIntegrationTest {
+
+	@Autowired ProductRepository repository;
+
+	/**
+	 * @since Step 6.1
+	 */
+	@Test
+	public void findsAllProducts() {
+
+		List<Product> products = repository.findAll();
+
+		assertThat(products).asList().hasSize(3);
+	}
+
+	/**
+	 * @since Step 6.2
+	 */
+	@Test
+	public void findsAllAppleProductPaged() {
+
+		Page<Product> products = repository.findByDescriptionContaining("Apple", new PageRequest(0, 1));
+
+		assertThat(products.isFirst()).isTrue();
+		assertThat(products.hasNext()).isTrue();
+
+		assertThat(products).filteredOn("name", "iPad").isNotEmpty();
+		assertThat(products).filteredOn("name", "Dock").isEmpty();
+	}
+
+	/**
+	 * @since Step 6.3
+	 */
+	@Test
+	public void returnsOptionalEmptyForNonExistingProduct() {
+
+		Optional<Product> result = repository.findOne(4711L);
+
+		assertThat(result).isEmpty();
+	}
+}
